a94e5dbbb502920c90f405bc461be8048a8993ec
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-01-21 18:51:13 -0300

    feat: add multiplication math method

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ const sub = (num1, num2) => {
   return num1 - num2;
 };
 
+const mult = (num1, num2) => {
+    return num1 * num2;
+};
+
 app.get("/add", (req, res) => {
     const sum = add(5, 3);
     res.send(sum);
 });
 
 app.get("/sub", (req, res) => {
-  const sub = sub(5, 3);
-  res.send(sub);
+  const total = sub(5, 3);
+  res.send(total);
+});
+
+app.get("/mult", (req, res) => {
+    const total = mult(5, 3);
+    res.send(total);
 });
 
 app.listen(3000, () => {

30d73236e8402dbc1aa35fff1da8cb6cc477f85c
pedroneto2 <pedro.neto992@gmail.com>
2022-01-21 18:04:19 -0300

    sub function added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-
 const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+const sub = (num1, num2) => {
+  return num1 - num2;
+};
+
+app.get("/sub", (req, res) => {
+  const sub = sub(5, 3);
+  res.send(sub);
 });
 
-
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});

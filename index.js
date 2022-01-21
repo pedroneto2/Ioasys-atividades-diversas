258ee5d54eb2e7ded210385367604c39c64af28a
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-01-21 14:57:21 -0300

    feat: add sum math method

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require("express");
 
 const app = express();
 
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+app.get("/add", (req, res) => {
+    const sum = add(5, 3);
+    res.send(sum);
+});
+
+
 app.listen(3000, () => {
     console.log("Listening on port 3000")
 });
